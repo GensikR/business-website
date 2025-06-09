76d0b9a0d79417580c2f0e36a256bfe236671397
Gensik Rubio <gensik2010@hotmail.com>
2025-06-09 18:30:14 -0500

    Revert "PLSSSS"
    This reverts commit a87e5557f8b6c0a5d3f0edc9ef5d35dc75eebd1a.
    
    PLEASE REVERT WORK

---
+++ my-app/src/components/admin/useFacebookSDK.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+
+declare global {
+  interface Window {
+    FB: any;
+    fbAsyncInit: () => void;
+  }
+}
+
+export default function useFacebookSDK(): boolean {
+  const [sdkReady, setSdkReady] = useState(false);
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    if (window.FB) {
+      setSdkReady(true);
+      return;
+    }
+
+    window.fbAsyncInit = () => {
+      window.FB.init({
+        appId: process.env.FACEBOOK_APP_ID!,
+        cookie: true,
+        xfbml: false,
+        version: 'v18.0',
+      });
+      setSdkReady(true);
+    };
+
+    if (!document.getElementById('facebook-jssdk')) {
+      const script = document.createElement('script');
+      script.id = 'facebook-jssdk';
+      script.src = 'https://connect.facebook.net/en_US/sdk.js';
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+    }
+  }, []);
+
+  return sdkReady;
+}
