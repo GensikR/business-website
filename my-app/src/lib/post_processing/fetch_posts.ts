5dc0885ebaa20b5b70f8807ea2124ac254e22af7
Gensik Rubio <gensik2010@hotmail.com>
2025-06-09 20:13:26 -0500

    We need to fetch more than 25 posts

--- my-app/src/lib/post_processing/fetch_posts.ts
+++ my-app/src/lib/post_processing/fetch_posts.ts
@@ -13,17 +13,28 @@ const fetchFacebookPosts = async ({
 }: FetchFacebookPostsParams) => {
   console.log("User Access Token:", userAccessToken);
 
-  try {
-    const postsRes = await axios.get(`https://graph.facebook.com/v23.0/${pageId}/posts`, {
-      params: {
-        access_token: pageAccessToken,
-        fields: 'id,message,created_time,full_picture,attachments{media_type,media,url,subattachments},permalink_url',
-      },
+  const allPosts: any[] = [];
+
+  let nextUrl: string | null = `https://graph.facebook.com/v23.0/${pageId}/posts?` +
+    new URLSearchParams({
+      access_token: pageAccessToken,
+      fields: 'id,message,created_time,full_picture,attachments{media_type,media,url,subattachments},permalink_url',
     });
 
-    const data = postsRes.data.data || postsRes.data;
+  try {
+    while (nextUrl) {
+      const response = await axios.get<{ data: any[]; paging?: { next?: string } }>(nextUrl);
+      const data: { data: any[]; paging?: { next?: string } } = response.data;
 
-    return data;
+      if (data?.data?.length) {
+        allPosts.push(...data.data);
+      }
+
+      nextUrl = data.paging?.next || null; // set next page URL or null if done
+    }
+
+    console.log(`Fetched ${allPosts.length} total posts.`);
+    return allPosts;
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
       console.error('Error fetching posts:', error.response?.data || error.message);
